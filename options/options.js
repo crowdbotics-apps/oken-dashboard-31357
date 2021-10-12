4d0848d4480650e310bb3f748278314e149bc69a
Crowdbotics <team@crowdbotics.com>
2021-10-12 20:33:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "oken_dashboard_31357",
+  url: "https://oken_dashboard_31357.botics.co",
+  api: "https://oken_dashboard_31357.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
